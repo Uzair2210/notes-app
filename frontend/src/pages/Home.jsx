dd075dfc20cf5d0d7ceee165b136fae23a6ecfe2
uzair2210 <uzair27140@gmail.com>
2023-10-13 19:38:09 +0500

    color

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -102,12 +102,12 @@ const Home = () => {
     <div className="max-w-3xl mx-auto">
       <form onSubmit={updateMode ? updateNotes : submitHandler} className="border-[1px] border-gray-200 my-10 p-4">
         <h2 className="text-2xl font-semibold mb-4">
-        {updateMode ? "Update":"Add"} <span className="text-gray-400">Notes</span>
+        {updateMode ? "Update":"Add"} <span className="text-[#19d4ca]">Notes</span>
         </h2>
 
         <input
           ref={titleInputTag}
-          className="block focus:outline-gray-600 w-full border-[1px] p-2 rounded-sm mb-2 border-gray-300"
+          className="block focus:outline-[#19d4ca] w-full border-[1px] p-2 rounded-sm mb-2 border-gray-300"
           type="text"
           placeholder="Title"
           value={title}
@@ -115,14 +115,14 @@ const Home = () => {
           required
         />
         <textarea
-          className="block focus:outline-gray-600 w-full border-[1px] p-2 mb-2 rounded-sm border-gray-300"
+          className="block focus:outline-[#19d4ca] w-full border-[1px] p-2 mb-2 rounded-sm border-gray-300"
           placeholder="Description"
           required
           value={desc}
           onChange={(e)=>setDesc(e.target.value)}
         />
 
-        <button className="py-1 px-2 bg-gray-600 rounded-sm text-white">
+        <button className="py-1 px-2 bg-[#19d4ca] rounded-sm text-white">
           {updateMode ? "Update":"Submit"}
         </button>
       </form>
@@ -130,7 +130,7 @@ const Home = () => {
       <div>
         {notes.map((v,i) => {
           return (
-            <div key={v._id} className="border-[1px] border-gray-200 p-4 my-4">
+            <div key={v._id} className="shadow-lg p-4 my-4">
               <h2 className="mb-1 font-semibold">{v.title}</h2>
               <p className="text-xs">
               {v.desc}
@@ -141,7 +141,7 @@ const Home = () => {
               </p>
 
               <div className="flex justify-end">
-                <i title="Edit" onClick={()=>setDataInForm(v)}  className="bx bx-edit text-gray-600 text-xl p-1 cursor-pointer"></i>
+                <i title="Edit" onClick={()=>setDataInForm(v)}  className="bx bx-edit text-blue-600 text-xl p-1 cursor-pointer"></i>
                 <i title="Delete" onClick={()=>window.confirm("Are you sure to delete this Note") && deleteNotes(v._id)} className="bx bx-trash text-red-700 text-xl p-1 cursor-pointer"></i>
               </div>
             </div>
